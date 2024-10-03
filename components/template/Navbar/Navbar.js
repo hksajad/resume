d46d0891ed8087beeb4063be2e5499a5393c814e
sajjad <sajjadhasanikakh06@gmail.com>
2024-10-03 23:40:20 +0330

    add

--- components/template/Navbar/Navbar.js
+++ components/template/Navbar/Navbar.js
@@ -1,13 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import "./Navbar.css";
+import Link from "next/link";
 const Navbar = () => {
   return (
-    <section id='navbar'>
-        <div className='container'>
-            <nav className='NavbarPage '></nav>
-        </div>
+    <section id="navbar">
+      <nav className="NavbarPage">
+        <ul className='menu flex justify-around items-center pt-3 pb-3'>
+          <div className='link flex gap-9'>
+            <Link href='/' className='font-bold'>صفحه اصلی</Link>
+            <Link href='/' className='font-bold'>درباره ما</Link>
+            <Link href='/' className='font-bold'>نمونه کار</Link>
+            <Link href='/' className='font-bold'>تماس با ما</Link>
+          </div>
+          <div className='dark_light'>سلام</div>
+        </ul>
+      </nav>
     </section>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
