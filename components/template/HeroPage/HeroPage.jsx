b85fd93ed2875935a04cffaa6493c65afc3fe3f1
sajjad <sajjadhasanikakh06@gmail.com>
2024-10-11 11:16:58 +0330

    add

--- components/template/HeroPage/HeroPage.jsx
+++ components/template/HeroPage/HeroPage.jsx
@@ -8,7 +8,7 @@ const HeroPage = () => {
         <div className="hero flex justify-around items-center w-[1200px] m-auto h-[100vh] mt-14 max-[768px]:flex-col max-[768px]:w-full max-[768px]:mt-32 max-[992px]:w-full max-[992px]:gap-10">
           {/* img hero */}
           <div className="img_hero">
-            <Image src='/img/imgg.png' width={500} height={500} className='w-[400px] h-[500px] border-[4px] border-[#1a1a1a]'/>
+            <Image src='/img/imgg.png' width={500} height={500} className='w-[400px] h-[500px] border-[4px] border-[#1a1a1a] max-[768px]:w-[300px] max-[768px]:h-[400px]'/>
           </div>
           {/* content hero */}
           <div className="content_hero w-[430px]  flex flex-col max-[768px]:text-center max-[992px]:w-[500px]">
